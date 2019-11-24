cf3e5fac6b620f7e4d593319e904f3350b0815fa
Kanokphat Jinanarong <kanokphatji@hotmail.com>
2019-11-24 16:38:51 +0700

    Change DB var to PascalCase, fix query

--- src/controller/routes/product.js
+++ src/controller/routes/product.js
@@ -45,12 +45,12 @@ ProductRouter.get('/:id', idCheck, async (req, res) => {
 
 // New product
 ProductRouter.post('/', async (req, res) => {
-    const {prodName, amountInStock, defaultPrice, prodType, image} = req.body;
-    if (!prodName) {
+    const {ProdName, AmountInStock, DefaultPrice, ProdType, Image} = req.body;
+    if (!ProdName) {
         return res.status(400).send({message: "Product name is not specified"});
     }
     try {
-        const result = await ProductRepository.createProduct(prodName, amountInStock, defaultPrice, prodType, image);
+        const result = await ProductRepository.createProduct(ProdName, AmountInStock, DefaultPrice, ProdType, Image);
         return res.status(201).send({
             message: "OK",
             id: result.insertId,
@@ -63,10 +63,10 @@ ProductRouter.post('/', async (req, res) => {
 
 ProductRouter.put('/:id', idCheck, async (req, res) => {
     const {id} = req.params;
-    const {prodName, amountInStock, defaultPrice, prodType, image} = req.body;
-    console.log({prodName, amountInStock, defaultPrice, prodType, image});
+    const {ProdName, AmountInStock, DefaultPrice, ProdType, Image} = req.body;
+    console.log({ProdName, AmountInStock, DefaultPrice, ProdType, Image});
     try {
-        const result = await ProductRepository.updateProduct(id, {prodName, amountInStock, defaultPrice, prodType, image});
+        const result = await ProductRepository.updateProduct(id, {ProdName, AmountInStock, DefaultPrice, ProdType, Image});
         return res.status(200).send({
             message: "OK"
         })
